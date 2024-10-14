7200cd30de52ffabcd6ca24c31e9458d7e3d4778
Homing So <i@homing.so>
2024-10-14 14:38:12 +0800

    refactor: modified the wrong name
    Signed-off-by: Homing So <i@homing.so>

--- assets/config/site.ts
+++ assets/config/site.ts
@@ -1,13 +1,13 @@
 export const siteConfig = {
-  name: 'ans-cn',
-  url: 'https://ans-cn.oio.sd',
-  ogImage: 'https://ans-cn.oio.sd/og.png',
+  name: 'asn-cn',
+  url: 'https://asn-cn.oio.sd',
+  ogImage: 'https://asn-cn.oio.sd/og.png',
   description: 'Collect all ANS of China',
-  keywords: ['ans-cn', 'ANS', 'China'],
+  keywords: ['asn-cn', 'ANS', 'China'],
   links: {
     author: 'https://homing.so/about',
     twitter: 'https://twitter.com/is_homingso',
-    github: 'https://github.com/oio-network/ans-cn',
+    github: 'https://github.com/oio-network/asn-cn',
   },
 }
 
