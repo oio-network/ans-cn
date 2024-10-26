5e7f72ad06a0bfad1cc2a3f5c3aec1804d0ccce0
Homing So <i@homing.so>
2024-10-26 11:37:36 +0800

    feat: filter ASNs by name
    Signed-off-by: Homing So <i@homing.so>

--- assets/components/data/data-table-toolbar.tsx
+++ assets/components/data/data-table-toolbar.tsx
@@ -21,9 +21,9 @@ export function DataTableToolbar<TData>({ table }: DataTableToolbarProps<TData>)
     <div className="flex items-center justify-between">
       <div className="flex flex-1 items-center space-x-2">
         <Input
-          placeholder="Filter tasks..."
-          value={(table.getColumn('title')?.getFilterValue() as string) ?? ''}
-          onChange={(event) => table.getColumn('title')?.setFilterValue(event.target.value)}
+          placeholder="Filter ASN..."
+          value={(table.getColumn('name')?.getFilterValue() as string) ?? ''}
+          onChange={(event) => table.getColumn('name')?.setFilterValue(event.target.value)}
           className="h-8 w-[150px] lg:w-[250px]"
         />
         {table.getColumn('isp') && (
